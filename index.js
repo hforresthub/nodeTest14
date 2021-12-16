7cd85d76624106f1ccfdfba21cd0297eeab4bb1e
Hal <hgrforrest@gmail.com>
2021-12-16 17:17:05 -0500

    emitter testing, on, off once and such

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ door.addListener('slam', (arg1, arg2, arg3) => {
 	console.log('slammed ', arg1, arg2, arg3)
 })
 
-door.addListener('pow', (arg1, arg2, arg3) => {
-	console.log('pow powed ', arg1, arg2, arg3)
+door.once('bang', (arg1, arg2, arg3) => {
+	console.log('bang bang ', arg1, arg2, arg3)
 })
+
+const powEventHandler = (arg1, arg2, arg3) => {
+	console.log('pow powed ', arg1, arg2, arg3)
+}
+
+const powListener = door.on('pow', powEventHandler)
 
 door.emit('slam', 1, 2, 3)
 door.emit('pow', 'a', 2, 3)
@@ -17,5 +23,22 @@ door.emit('slam')
 const events = door.eventNames()
 console.log(events)
 
-console.log(door.getMaxListeners())
+console.log("cur max num listeners: ", door.getMaxListeners())
 
+door.setMaxListeners(20)
+
+console.log("cur max num listeners: ", door.getMaxListeners())
+
+setTimeout(() => {
+	console.log("total num slam listeners: ", door.listenerCount('slam'))
+}, 1000)
+
+console.log("list of pow listeners: ", door.listeners('pow'))
+
+door.off('pow', powEventHandler)
+
+console.log("total num pow listeners: ", door.listenerCount('pow'))
+
+door.emit('bang')
+door.emit('bang')
+door.emit('bang')
